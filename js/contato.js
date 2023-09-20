3b2cc05d0c8d139d9a0b882d08583e94c4b5f245
Lucas Bomfim <lucasbomfimsobral@gmail.com>
2023-09-20 11:02:03 -0300

    Adicionei um Feedback quando enviar o forms.

--- js/contato.js
+++ js/contato.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
     $('#numero').mask('(00) 00000-0000');
+
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+        //para fazer só aparecer:
+        //$(".pedido").show();
+        $(".pedido").fadeIn(1000);
+        $("#plane").fadeIn(3000);
+        // para remover:
+        // $(".pedido").removeAttr("style").hide();
+        $("#email").val("");
+        $("#numero").val("");
+        $("#descrição").val("");
+    })
 })
